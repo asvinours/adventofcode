af2dfe0b1a3a5edb314f7cd17f5481b42dfaccf0
captain <Fabrice Baumann>
2020-12-09 19:35:35 -0500

    Refactor day_1

--- 2020/day_1_solution.py
+++ 2020/day_1_solution.py
@@ -1,23 +1,20 @@
 #!python
 
+from itertools import combinations
+
 with open('day_1_input.txt') as f:
     lines = f.read().splitlines()
 
 def sum(*args):
     return reduce((lambda x, y: x + y), args)
 
+def product(*args):
+    return reduce((lambda x, y: x * y), args)
+
 expenses = list(map(int, lines))
+combs = combinations(expenses, 3)
+for c in combs:
+    if sum(*c) == 2020:
+        print(product(*c))
+        break
 
-for i in range(len(expenses)-1):
-    for j in range(len(expenses)-1):
-        if i == j:
-            continue
-
-        for k in range(len(expenses)-1):
-            if i == k or k == j:
-                continue
-
-            sumsum = sum(expenses[i], expenses[j], expenses[k])
-            if sumsum == 2020:
-                print(expenses[i] * expenses[j] * expenses[k])
-                exit(0)
